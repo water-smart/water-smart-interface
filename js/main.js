a4ef9c8111c5e86d4c8f4a265844d5469d0a8d5d
jack <47188744+jackgalipeault@users.noreply.github.com>
2019-03-22 13:23:08 -0400

    html, css, js initialization

---
+++ js/main.js
@@ -0,0 +1,108 @@
+window.onload = function () {
+  
+    var questionArea = document.getElementsByClassName('questions')[0],
+        answerArea   = document.getElementsByClassName('answers')[0],
+        checker      = document.getElementsByClassName('checker')[0],
+        current      = 0,
+    
+       // An object that holds all the questions + possible answers.
+       // In the array --> last digit gives the right answer position
+        allQuestions = {
+          'What is the recommended amount of water a human should drink per day?' : ['16 cups', '8 cups', '10 cups', '5 cups', 1],
+          
+          'How much water does the average Canadian use per day?' : ['329L', '500L' , '238L', '1239L', 0],
+                  
+                  'Which fruit or vegetable requires the most water to produce?' : ['Blueberries', 'Olives', 'Mangos', 'Corn', 1],
+          
+          'How long can a human survive without water?' : ['14 days', '6 days', '3 days', '1 day', 2],
+          
+          'How many cups of water do you need to make a box of Kraft Dinner?' : ['6 cups', '3 cups' , '1.5 cups', '8 cups', 0],
+                  
+                  'What is the chemical composition of water?' : ['H2O', 'CO2', 'O2', 'H2O2', 0],
+        };
+        
+    function loadQuestion(curr) {
+    // This function loads all the question into the questionArea
+    // It grabs the current question based on the 'current'-variable
+    
+      var question = Object.keys(allQuestions)[curr];
+      
+      questionArea.innerHTML = '';
+      questionArea.innerHTML = question;    
+    }
+    
+    function loadAnswers(curr) {
+    // This function loads all the possible answers of the given question
+    // It grabs the needed answer-array with the help of the current-variable
+    // Every answer is added with an 'onclick'-function
+    
+      var answers = allQuestions[Object.keys(allQuestions)[curr]];
+      
+      answerArea.innerHTML = '';
+      
+      for (var i = 0; i < answers.length -1; i += 1) {
+        var createDiv = document.createElement('div'),
+            text = document.createTextNode(answers[i]);
+        
+        createDiv.appendChild(text);      
+        createDiv.addEventListener("click", checkAnswer(i, answers));
+        
+        
+        answerArea.appendChild(createDiv);
+      }
+    }
+    
+    function checkAnswer(i, arr) {
+      // This is the function that will run, when clicked on one of the answers
+      // Check if givenAnswer is sams as the correct one
+      // After this, check if it's the last question:
+      // If it is: empty the answerArea and let them know it's done.
+      
+      return function () {
+        var givenAnswer = i,
+            correctAnswer = arr[arr.length-1];
+        
+        if (givenAnswer === correctAnswer) {
+          addChecker(true);             
+        } else {
+          addChecker(false);                        
+        }
+        
+        if (current < Object.keys(allQuestions).length -1) {
+          current += 1;
+          
+          loadQuestion(current);
+          loadAnswers(current);
+        } else {
+          questionArea.innerHTML = 'Done';
+          answerArea.innerHTML = '';
+        }
+                                
+      };
+    }
+    
+    function addChecker(bool) {
+    // This function adds a div element to the page
+    // Used to see if it was correct or false
+    
+      var createDiv = document.createElement('div'),
+          txt       = document.createTextNode(current + 1);
+      
+      createDiv.appendChild(txt);
+      
+      if (bool) {
+        
+        createDiv.className += 'correct';
+        checker.appendChild(createDiv);
+      } else {
+        createDiv.className += 'false';
+        checker.appendChild(createDiv);
+      }
+    }
+    
+    
+    // Start the quiz right away
+    loadQuestion(current);
+    loadAnswers(current);
+    
+  };
