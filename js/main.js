b719bba5dfd826128e61c961c40d449323673432
Kyle Abrahams <kyleagbc@gmail.com>
2019-04-05 14:01:27 -0400

    sounds added

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ function startButton(){
   document.getElementById("nextButton").style.display = 'block';
   document.getElementById("scenarioTitle").style.display = 'block';
   document.getElementById("scenarioBody").style.display = 'block';
+  // audio toggle
+
 }
 
 function nextButton(){
@@ -16,9 +18,59 @@ function nextButton(){
   document.getElementById("nextButton").style.display = 'none';
   document.getElementById("scenarioTitle").style.display = 'none';
   document.getElementById("scenarioBody").style.display = 'none';
-  document.getElementById("quiz").style.display = 'none';
-  document.getElementById("middlemain").style.display = 'grid';
+  // document.getElementById("quiz").style.display = 'none';
+  // document.getElementById("sequence").style.display = 'grid';
 }
+//SOUND Start
+let listSound = [
+  "3 beep high", // 0
+  "3 beep low", // 1
+  "Bell", // 2
+  "Boom", // 3
+  "Correct", // 4
+  "Ding", // 5
+  "Drop", // 6
+  "Gong", // 7
+  "Music Short", // 8
+  "Piano Down", // 9
+  "Piano Up", // 10
+  "Sticks Strings", // 11
+  "Synth down", // 12
+  "Synth stab", // 13
+  "String stab", // 14
+  "robot up", // 15
+  "robot down", // 16
+  "robot spam", // 17
+  "Loser", // 18
+  "Loser slow", // 19
+  "Doorbell", // 20
+  "Horns", // 21
+  "5 robot signals", // 22
+  "long robot spam", // 23
+  "4 robot signals", // 24
+  "3 String stabs" // 25
+
+
+];
+document.querySelector('#startButton').addEventListener('click',startSound);
+//Next sound below
+document.querySelector('#nextButton').addEventListener('click',nextSound);
+
+
+
+//Start / Next button sound
+function startSound(){
+  let track = new Audio();
+  track.src = "sound/" +listSound[14] + ".wav";
+  track.paused ? track.play() : track.pause();
+}
+
+function nextSound(){
+  let track = new Audio();
+  track.src = "sound/" +listSound[25] + ".wav";
+  track.paused ? track.play() : track.pause();
+}
+//SOUND End
 
 window.onload = function () {
   
@@ -36,7 +88,7 @@ window.onload = function () {
                   
           'Which fruit or vegetable requires the most water to produce?' : ['Blueberries', 'Olives', 'Mangos', 'Corn', 1],
           
-          'How long can a human survive without water?' : ['14 days', '6 days', '3 days', '1 day', 2],
+          'How many days can a human survive without water?' : ['14 days', '6 days', '3 days', '1 day', 2],
           
           'How many cups of water do you need to make a box of Kraft Dinner?' : ['6 cups', '3 cups' , '1.5 cups', '8 cups', 0],
                   
@@ -51,6 +103,9 @@ window.onload = function () {
       
       questionArea.innerHTML = '';
       questionArea.innerHTML = question;    
+      document.getElementById("sequence").style.display = 'none';
+      document.getElementById("checker").style.display = 'none';
+
     }
     
     function loadAnswers(curr) {
@@ -61,6 +116,7 @@ window.onload = function () {
       var answers = allQuestions[Object.keys(allQuestions)[curr]];
       
       answerArea.innerHTML = '';
+
       
       for (var i = 0; i < answers.length -1; i += 1) {
         var createDiv = document.createElement('div'),
@@ -79,25 +135,43 @@ window.onload = function () {
       // Check if givenAnswer is sams as the correct one
       // After this, check if it's the last question:
       // If it is: empty the answerArea and let them know it's done.
+
       
       return function () {
         var givenAnswer = i,
             correctAnswer = arr[arr.length-1];
-        
+
+      // below triggers screen to disappear instead of say 'Done'     
+            //  wrapperArea  = document.getElementsByClassName('wrapper')[0];
+
         if (givenAnswer === correctAnswer) {
           addChecker(true);             
+         // window.alert("wrong");
+          let track = new Audio();
+          track.src = "sound/" +listSound[21] + ".wav";
+          track.paused ? track.play() : track.pause();
+
         } else {
-          addChecker(false);                        
+          addChecker(false);
+        //  window.alert("wrong");
+          let track = new Audio();
+          track.src = "sound/" +listSound[18] + ".wav";
+          track.paused ? track.play() : track.pause();
+                
         }
         
         if (current < Object.keys(allQuestions).length -1) {
           current += 1;
-          
           loadQuestion(current);
           loadAnswers(current);
         } else {
           questionArea.innerHTML = 'Done';
           answerArea.innerHTML = '';
+    // below triggers screen to disappear instead of say 'Done'     
+          // wrapperArea.innerHTML = '';
+          document.getElementById("sequence").style.display = 'grid';
+          document.getElementById("wrapper").style.display = 'none';
+
         }
                                 
       };
